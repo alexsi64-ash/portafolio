594d0846f11fbd0d18f45b97687124ade848fe6d
Alexander Silva <alexandersilva580@outlook.com>
2026-02-05 13:45:13 -0400

    redaccion de informacion del cv a constantes para reutilizar

---
+++ src/data/cvData.ts
@@ -0,0 +1,54 @@
+export const personalInfo = {
+  nombre: "Alexander Silva Huanca",
+  titulo: "Técnico Superior en Sistemas Informáticos",
+  direccion: "Santa Cruz de la Sierra, Bolivia",
+  email: "alexandersilva580@outlook.com",
+  celular: "71319316",
+  domicilio: "km 9 Doble Vía la Guardia Urbanización Los Piyos",
+};
+
+export const educacion = [
+  {
+    grado: "Técnico Superior en Sistemas Informáticos",
+    instituto: "INCOS Santa Cruz", 
+    periodo: "2023 - 2025", 
+    status: "Egresado / Titulación en trámite"
+  }
+];
+
+export const reconocimiento = [
+  {
+    titulo: "Primer Lugar en la Feria Productiva de Proyectos de Innovación 2025", 
+    institucion: "Instituto Técnico INCOS Santa Cruz", 
+    fecha: "Septiembre 2025" 
+  },
+  {
+    titulo: "Diploma de Excelencia Académica", 
+    institucion: "3ro 'A' de la Carrera de Sistemas Informáticos - INCOS", 
+    fecha: "Septiembre 2025" 
+  },
+  {
+    titulo: "Tercer Lugar en la Feria de Proyectos de Innovación", 
+    institucion: "INCOS Santa Cruz", 
+    fecha: "Septiembre 2024" 
+  },
+  {
+    titulo: "Segundo Lugar en la Feria de Proyectos Productivos de Innovación de Primer Año", 
+    institucion: "INCOS Santa Cruz", 
+    fecha: "Septiembre 2023" 
+  }
+];
+
+export const skills = {
+  lenguajes: ["HTML", "CSS", "JavaScript", "PHP", "C++", "Java"], 
+  frameworks: ["React", "Tailwind CSS", "Bootstrap", "Laravel"], 
+  herramientas: ["Git", "GitHub", "Docker", "SQL", "Android Studio", "AutoCAD"], 
+  design: ["Adobe Photoshop", "Illustrator"],
+  os: ["Windows 7/10/11", "Fedora Linux", "Debian Linux", "Ubuntu Linux"] 
+};
+
+export const cursos = [
+  { nombre: "TypeScript Profesional", plataforma: "Codigofacilito", fecha: "Julio 2025" }, 
+  { nombre: "React con Ejercicios Prácticos", plataforma: "Codigofacilito", fecha: "Julio 2025" },
+  { nombre: "Ciberseguridad", plataforma: "ClaseFlix", fecha: "Enero 2025" } 
+];
